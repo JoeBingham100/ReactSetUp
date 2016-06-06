285d833d48c8e8b4edde8b1b5afb71c917580c62
Joseph Bingham <Joseph Bingham>
2016-06-06 13:42:14 -0700

    Add Existing File

---
+++ react-project/app/components/home.js
@@ -0,0 +1,14 @@
+/**
+ * Created by The Admin on 6/6/2016.
+ */
+var React = require('react');
+
+var Home = React.createClass({
+    render: function () {
+        return (
+            <div> Hello from Home! </div>
+        )
+    }
+});
+
+module.exports = Home;
